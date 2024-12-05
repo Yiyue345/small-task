7dc91e8817b7a0e4b6149c6ff8eb57e364fe471c
YiYue <dyx060312@163.com>
2024-12-05 22:35:15 +0800

    有了关于

--- app/src/main/java/com/example/smalltask/activities/AboutActivity.kt
+++ app/src/main/java/com/example/smalltask/activities/AboutActivity.kt
@@ -1,6 +1,8 @@
 package com.example.smalltask.activities
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -26,6 +28,17 @@ class AboutActivity : BaseActivity() {
 
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = ""
+        supportActionBar?.title = getString(R.string.about_text)
+
+        binding.nothingBtn.setOnClickListener {
+            Toast.makeText(this, getString(R.string.no_update), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish()
+        }
+        return true
     }
 }
