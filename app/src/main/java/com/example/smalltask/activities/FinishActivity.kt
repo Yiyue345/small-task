944b371869f8af27948a5d2c1ac5903b7f4703bb
YiYue <dyx060312@163.com>
2024-12-07 11:11:30 +0800

    这才是最后一次更新：修改了README.md，以及之前忘记做的东西

--- app/src/main/java/com/example/smalltask/activities/FinishActivity.kt
+++ app/src/main/java/com/example/smalltask/activities/FinishActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.smalltask.BaseActivity
 import com.example.smalltask.R
 import com.example.smalltask.databinding.ActivityFinishBinding
+import com.example.smalltask.learning.MyDatabaseHelper
 
 class FinishActivity : BaseActivity() {
 
@@ -27,6 +28,12 @@ class FinishActivity : BaseActivity() {
         val mode = intent.getStringExtra("mode")
         val counts = intent.getIntExtra("counts", 0)
 
+        val getInfo = getSharedPreferences("latest", MODE_PRIVATE)
+        val userName = getInfo.getString("username", "")!!
+
+        val dbHelper = MyDatabaseHelper(this, "Database${userName}.db", 1)
+        val db = dbHelper.writableDatabase
+
         binding.finishCountsText.text = getString(R.string.finish_counts, counts)
 
         binding.finishBtn.setOnClickListener {
@@ -40,7 +47,31 @@ class FinishActivity : BaseActivity() {
             }
         }
         else if (mode == "review") {
-            binding.moreBtn.visibility = View.GONE
+            val userWordCursor = db.query("UserWord", null, null, null, null, null, null)
+            var i = 0
+            if (userWordCursor.moveToFirst()) {
+
+                do {
+                    val lastTime = userWordCursor.getLong(userWordCursor.getColumnIndexOrThrow("lastTime"))
+                    val times = userWordCursor.getInt(userWordCursor.getColumnIndexOrThrow("times"))
+                    val today = (System.currentTimeMillis() / 86400000L) * 86400000L
+                    if (today > lastTime + (times - 1) / 2 * today) { // (time-1)/2 可能还能改
+                        i++
+                    }
+                } while (userWordCursor.moveToNext())
+            }
+            userWordCursor.close()
+            if (i > 0) {
+                binding.moreBtn.setOnClickListener {
+                    val intent = Intent(this, ReviewActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+            else {
+                binding.moreBtn.visibility = View.GONE
+            }
+
         }
 
     }
